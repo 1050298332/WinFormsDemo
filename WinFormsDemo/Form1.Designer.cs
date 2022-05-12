65e204a74813b228f4d3dffe0fef4b9a66a5e8bf
Aclles <1050298332@qq.com>
2022-05-12 15:12:19 +0800

    添加定时器 锁屏/解锁 事件 摄像头优化

--- WinFormsDemo/Form1.Designer.cs
+++ WinFormsDemo/Form1.Designer.cs
@@ -39,12 +39,10 @@
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.主页ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.设置1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.设置2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.退出ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.uiStyleManager1 = new Sunny.UI.UIStyleManager(this.components);
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
@@ -60,14 +58,14 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 35);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1384, 716);
+            this.panel1.Size = new System.Drawing.Size(832, 615);
             this.panel1.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.pictureBox1.Image = null;
-            this.pictureBox1.Location = new System.Drawing.Point(890, 328);
+            this.pictureBox1.Location = new System.Drawing.Point(896, 163);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(388, 322);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -78,7 +76,7 @@
             // 
             this.Button2.Enabled = false;
             this.Button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Button2.Location = new System.Drawing.Point(1076, 199);
+            this.Button2.Location = new System.Drawing.Point(1109, 39);
             this.Button2.MinimumSize = new System.Drawing.Size(1, 1);
             this.Button2.Name = "Button2";
             this.Button2.Size = new System.Drawing.Size(175, 61);
@@ -89,14 +87,15 @@
             // 
             // Button1
             // 
+            this.Button1.Enabled = false;
             this.Button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Button1.Location = new System.Drawing.Point(847, 199);
+            this.Button1.Location = new System.Drawing.Point(615, 36);
             this.Button1.MinimumSize = new System.Drawing.Size(1, 1);
             this.Button1.Name = "Button1";
             this.Button1.Size = new System.Drawing.Size(175, 61);
             this.Button1.Style = Sunny.UI.UIStyle.Black;
             this.Button1.TabIndex = 3;
-            this.Button1.Text = "拍照";
+            this.Button1.Text = "验证";
             this.Button1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             this.Button1.Click += new System.EventHandler(this.Button1_Click);
             // 
@@ -106,20 +105,16 @@
             this.uiComboBox1.FillColor = System.Drawing.Color.White;
             this.uiComboBox1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.uiComboBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.uiComboBox1.Items.AddRange(new object[] {
-            "摄像头1",
-            "摄像头2"});
-            this.uiComboBox1.Location = new System.Drawing.Point(155, 22);
+            this.uiComboBox1.Location = new System.Drawing.Point(238, 39);
             this.uiComboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uiComboBox1.MinimumSize = new System.Drawing.Size(63, 0);
             this.uiComboBox1.Name = "uiComboBox1";
             this.uiComboBox1.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
             this.uiComboBox1.Radius = 15;
             this.uiComboBox1.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(58)))), ((int)(((byte)(92)))));
-            this.uiComboBox1.Size = new System.Drawing.Size(262, 51);
+            this.uiComboBox1.Size = new System.Drawing.Size(262, 32);
             this.uiComboBox1.Style = Sunny.UI.UIStyle.Black;
             this.uiComboBox1.TabIndex = 2;
-            this.uiComboBox1.Text = "摄像头1";
             this.uiComboBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
             this.uiComboBox1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             this.uiComboBox1.SelectedIndexChanged += new System.EventHandler(this.uiComboBox1_SelectedIndexChanged);
@@ -127,9 +122,9 @@
             // videoSourcePlayer1
             // 
             this.videoSourcePlayer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.videoSourcePlayer1.Location = new System.Drawing.Point(56, 92);
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(26, 103);
             this.videoSourcePlayer1.Name = "videoSourcePlayer1";
-            this.videoSourcePlayer1.Size = new System.Drawing.Size(648, 435);
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(779, 484);
             this.videoSourcePlayer1.TabIndex = 1;
             this.videoSourcePlayer1.Text = "videoSourcePlayer1";
             this.videoSourcePlayer1.VideoSource = null;
@@ -147,25 +142,16 @@
             this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.退出ToolStripMenuItem,
-            this.主页ToolStripMenuItem,
             this.设置ToolStripMenuItem,
             this.退出ToolStripMenuItem1});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(169, 140);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(127, 106);
             // 
             // 退出ToolStripMenuItem
             // 
             this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
-            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(168, 34);
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(126, 34);
             this.退出ToolStripMenuItem.Text = "登录";
-            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
-            // 
-            // 主页ToolStripMenuItem
-            // 
-            this.主页ToolStripMenuItem.Name = "主页ToolStripMenuItem";
-            this.主页ToolStripMenuItem.Size = new System.Drawing.Size(168, 34);
-            this.主页ToolStripMenuItem.Text = "临时离开";
-            this.主页ToolStripMenuItem.Click += new System.EventHandler(this.临时离开ToolStripMenuItem_Click);
             // 
             // 设置ToolStripMenuItem
             // 
@@ -173,40 +159,39 @@
             this.设置1ToolStripMenuItem,
             this.设置2ToolStripMenuItem});
             this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
-            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(168, 34);
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(126, 34);
             this.设置ToolStripMenuItem.Text = "设置";
             // 
             // 设置1ToolStripMenuItem
             // 
             this.设置1ToolStripMenuItem.Name = "设置1ToolStripMenuItem";
-            this.设置1ToolStripMenuItem.Size = new System.Drawing.Size(255, 40);
-            this.设置1ToolStripMenuItem.Text = "临时离开密码";
+            this.设置1ToolStripMenuItem.Size = new System.Drawing.Size(183, 40);
+            this.设置1ToolStripMenuItem.Text = "1";
             // 
             // 设置2ToolStripMenuItem
             // 
             this.设置2ToolStripMenuItem.Name = "设置2ToolStripMenuItem";
-            this.设置2ToolStripMenuItem.Size = new System.Drawing.Size(255, 40);
+            this.设置2ToolStripMenuItem.Size = new System.Drawing.Size(183, 40);
             this.设置2ToolStripMenuItem.Text = "设置2";
             // 
             // 退出ToolStripMenuItem1
             // 
             this.退出ToolStripMenuItem1.Name = "退出ToolStripMenuItem1";
-            this.退出ToolStripMenuItem1.Size = new System.Drawing.Size(168, 34);
+            this.退出ToolStripMenuItem1.Size = new System.Drawing.Size(126, 34);
             this.退出ToolStripMenuItem1.Text = "退出";
             this.退出ToolStripMenuItem1.Click += new System.EventHandler(this.退出ToolStripMenuItem1_Click);
-            // 
-            // uiStyleManager1
-            // 
-            this.uiStyleManager1.DPIScale = true;
-            this.uiStyleManager1.Style = Sunny.UI.UIStyle.LayuiOrange;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(1384, 751);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(832, 650);
+            this.ContextMenuStrip = this.contextMenuStrip1;
             this.ControlBox = false;
             this.ControlBoxFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
             this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.Font = new System.Drawing.Font("微软雅黑", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.White;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -220,7 +205,7 @@
             this.Style = Sunny.UI.UIStyle.Black;
             this.Text = "测试应用";
             this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.TopMost = true;
             this.ZoomScaleRect = new System.Drawing.Rectangle(26, 26, 1384, 751);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -240,8 +225,6 @@
         private ToolStripMenuItem 设置1ToolStripMenuItem;
         private ToolStripMenuItem 设置2ToolStripMenuItem;
         private ToolStripMenuItem 退出ToolStripMenuItem1;
-        private ToolStripMenuItem 主页ToolStripMenuItem;
-        public Sunny.UI.UIStyleManager uiStyleManager1;
         private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
         private Sunny.UI.UIComboBox uiComboBox1;
         private Sunny.UI.UIButton Button2;
--- WinFormsDemo/Form1.cs
+++ WinFormsDemo/Form1.cs
@@ -1,28 +1,72 @@
-using Sunny.UI;
 using AForge.Video.DirectShow;
+using Microsoft.Win32;
+using Sunny.UI;
 
 namespace WinFormsDemo
 {
     public partial class Form1 : UIForm
     {
+        public static Form1 form1;
         FilterInfoCollection videoDevices;//摄像头设备集合
         VideoCaptureDevice videoSource;//捕获设备源
-        //Bitmap img;//处理图片
+                                       //Bitmap img;//处理图片
 
+        System.Timers.Timer t = new System.Timers.Timer(1800000);//实例化Timer类，设置间隔时间为10000毫秒；
+        //30分钟为1800000毫秒
         public Form1()
         {
+            SessionSwitchClass SessionCheck = new SessionSwitchClass();
 
+            t.Elapsed += new System.Timers.ElapsedEventHandler(Execute);//到达时间的时候执行事件；
+            t.AutoReset = false;//设置是执行一次（false）还是一直执行(true)；
+            t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+            t.Stop(); //先关闭定时器
             InitializeComponent();
+            form1 = this;//再把主窗体赋值给form1
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;//设置该属性 为false
             //最大化窗体
             //this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
         }
+        private void  Execute(object source, System.Timers.ElapsedEventArgs e)
+        {
+            t.Stop(); //先关闭定时器
+                      // 正常显示窗体
+            this.Visible = true;
+            ShutCamera();//释放摄像头
+            if (uiComboBox1.Text == "摄像头1" && videoDevices.Count > 0)
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            else if (uiComboBox1.Text == "摄像头2" && videoDevices.Count > 1)
+                videoSource = new VideoCaptureDevice(videoDevices[1].MonikerString);
+            else
+            {
+                MessageBox.Show("选择的摄像头不存在！！！");
+                return;
+            }
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSourcePlayer1.Start();
 
+            Button1.Enabled = true;//开启“识别功能”
+        }
         //双击托盘图标事件
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            // 正常显示窗体
+            t.Stop(); //先关闭定时器
+                      // 正常显示窗体
             this.Visible = true;
-            this.WindowState = FormWindowState.Maximized;
+            ShutCamera();//释放摄像头
+            if (uiComboBox1.Text == "摄像头1" && videoDevices.Count > 0)
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            else if (uiComboBox1.Text == "摄像头2" && videoDevices.Count > 1)
+                videoSource = new VideoCaptureDevice(videoDevices[1].MonikerString);
+            else
+            {
+                MessageBox.Show("选择的摄像头不存在！！！");
+                return;
+            }
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSourcePlayer1.Start();
+
+            Button1.Enabled = true;//开启“识别功能”
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -42,31 +86,33 @@ namespace WinFormsDemo
             Application.Exit();
         }
 
-        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //webView2.Source =  new Uri("http://mccsdl.top/login");
-        }
-
-        private void 临时离开ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //临时离开后暂停定时扫描识别
-            //打开form锁屏页面
-            Form2 f2 = new Form2();
-            f2.Show();
-            //webView2.Source = new Uri("http://mccsdl.top");
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
+
             // 窗体启动事件代码
             //RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             //registryKey.SetValue("BaichuiMonitor", Application.ExecutablePath);//"BaichuiMonitor"可以自定义
             //加载时将窗口置于顶层
-            this.TopMost = true;    
+            this.TopMost = true;
             //取消窗口边框 在form属性设置中window Style
             //先检测电脑所有的摄像头
             videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             //MessageBox.Show("检测到了" + videoDevices.Count.ToString() + "个摄像头！");
+            if (videoDevices.Count == 0)
+            {
+                MessageBox.Show("检测到电脑没有摄像头-请安装摄像头");
+                //关闭程序
+                Application.Exit();
+            }
+            else
+            {
+                String a = videoDevices.Count.ToString();//1 = 1个
+
+                for (int b = 1;b<=a.ToInt();b++){
+                    uiComboBox1.Items.Add("摄像头"+b);
+                }
+                uiComboBox1.Text = "摄像头1";
+            }
         }
 
         private void uiComboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -84,7 +130,7 @@ namespace WinFormsDemo
             videoSourcePlayer1.VideoSource = videoSource;
             videoSourcePlayer1.Start();
 
-            Button1.Enabled = true;//开启“拍摄功能”
+            Button1.Enabled = true;//开启“识别功能”
         }
 
         // 关闭并释放摄像头
@@ -98,19 +144,26 @@ namespace WinFormsDemo
             }
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private async void Button1_Click(object sender, EventArgs e)
         {
-                //img = videoSourcePlayer1.GetCurrentVideoFrame();//拍摄
-                //pictureBox1.Image = img;
-                //Button2.Enabled = true;//开启“保存”功能
-            
+            //img = videoSourcePlayer1.GetCurrentVideoFrame();//拍摄
+            //pictureBox1.Image = img;
+            //Button2.Enabled = true;//开启“保存”功能
+            // 隐藏窗体重新计时 定时开启
+            //点击后进行验证  验证完成后隐藏并关闭摄像头
+
+            await Task.Delay(3000); //模仿验证时间
+            MessageBox.Show("识别完成");
+            this.Visible = false;
+            ShutCamera();//释放摄像头
+            t.Start(); //执行完毕后再开启器
 
         }
 
         //private void Button2_Click(object sender, EventArgs e)
         //{
         //    //"保存"按钮click事件
-       
+
         //        try
         //        {
         //            //以当前时间为文件名，保存为jpg格式
@@ -126,5 +179,88 @@ namespace WinFormsDemo
         //            MessageBox.Show(ex.Message);
         //        }
         //}
+        private class SessionSwitchClass 
+        {
+            /// <summary>
+            /// 解屏后执行的委托
+            /// </summary>
+            public Action SessionUnlockAction { get; set; }
+
+            /// <summary>
+            /// 锁屏后执行的委托
+            /// </summary>
+            public Action SessionLockAction { get; set; }
+
+            public SessionSwitchClass()
+            {
+                SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+            }
+
+            //析构，防止句柄泄漏
+            ~SessionSwitchClass()
+            {
+                //Do this during application close to avoid handle leak
+                SystemEvents.SessionSwitch -= SystemEvents_SessionSwitch;
+            }
+            //public string UnlockOrLock = "未触发";///这句是自己的代码
+            //当前登录的用户变化（登录、注销和解锁屏）
+            public void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+            {
+                switch (e.Reason)
+                {
+                    //用户登录
+                    case SessionSwitchReason.SessionLogon:
+                        BeginSessionUnlock();
+                        break;
+                    //解锁屏
+                    case SessionSwitchReason.SessionUnlock:
+                        BeginSessionUnlock();
+                        break;
+                    //锁屏
+                    case SessionSwitchReason.SessionLock:
+                        BeginSessionLock();
+                        break;
+                    //注销
+                    case SessionSwitchReason.SessionLogoff:
+                        break;
+                }
+            }
+
+            /// <summary>
+            /// 解屏、登录后执行
+            /// </summary>
+            private void BeginSessionUnlock()
+            {
+                //解屏、登录后执行
+                form1.t.Stop(); //先关闭定时器
+                          // 正常显示窗体
+                form1.Visible = true;
+                //====================================================
+                form1.ShutCamera();//释放摄像头
+                if (form1.uiComboBox1.Text == "摄像头1" && form1.videoDevices.Count > 0)
+                    form1.videoSource = new VideoCaptureDevice(form1.videoDevices[0].MonikerString);
+                else if (form1.uiComboBox1.Text == "摄像头2" && form1.videoDevices.Count > 1)
+                    form1.videoSource = new VideoCaptureDevice(form1.videoDevices[1].MonikerString);
+                else
+                {
+                    MessageBox.Show("选择的摄像头不存在！！！");
+                    return;
+                }
+                form1.videoSourcePlayer1.VideoSource = form1.videoSource;
+                form1.videoSourcePlayer1.Start();
+
+                form1.Button1.Enabled = true;//开启“拍摄功能”
+            }
+
+            /// <summary>
+            /// 锁屏后执行
+            /// </summary>
+            private void BeginSessionLock()
+            {
+                //锁屏后执行
+                form1.t.Stop(); //先关闭定时器
+            }
+        }
+
     }
 }
